336041968c4784f9e4c2dead6afa331634ed9a03
Reid Ransom <reid@reidransom.com>
2013-10-24 11:50:40 -0700

    converted tabs to spaces

--- make.py
+++ make.py
@@ -7,27 +7,28 @@ def usage():
     return 'python make.py [stage|serve]\n'
 
 def bash(cmd):
-	subprocess.call(cmd, shell=True)
+    subprocess.call(cmd, shell=True)
 
 def rsync(src, dest, exclude=['*.pyc'], args=['--checksum', '--archive', '--verbose', '--delete', '--delete-excluded']):
-	exclude.append('.DS_Store')
-	args.extend(map(lambda s: '--exclude ' + s, exclude))
-	args = ' '.join(args)
-	cmd = 'rsync %(args)s %(src)s %(dest)s' % locals()
-	print cmd
-	bash(cmd)
+    exclude.append('.DS_Store')
+    args.extend(map(lambda s: '--exclude ' + s, exclude))
+    args = ' '.join(args)
+    cmd = 'rsync %(args)s %(src)s %(dest)s' % locals()
+    print cmd
+    bash(cmd)
 
 def stage():
-	rsync(
-		'reiduploader/',
-		'reidransom@reidransom.com:webapps/upload_wsgi/reiduploader/'
-	)
-	rsync(
-		'webfaction/',
-		'reidransom@reidransom.com:webapps/upload_wsgi/',
-		args=['--archive', '--checksum', '--verbose']
-	)
-	bash('ssh reidransom@reidransom.com ./webapps/upload_wsgi/apache2/bin/restart')
+    rsync(
+        'reiduploader/',
+        'reidransom@reidransom.com:webapps/upload_wsgi/reiduploader/'
+    )
+    rsync(
+        'webfaction/',
+        'reidransom@reidransom.com:webapps/upload_wsgi/',
+        args=['--archive', '--checksum', '--verbose']
+    )
+    bash('ssh reidransom@reidransom.com ./webapps/upload_wsgi/apache2/bin/restart')
+
 def serve():
     from reiduploader.views import app
     from reiduploader.settings import PORT
